a78575b7e706dfab9b66458b4f08a9504036763c
simbu95 <simbu.tanksley@yahoo.com>
2022-02-05 11:06:34 -0600

    Update AutoTrack_Min.lua

--- AutoTracker/AutoTrack_Min.lua
+++ AutoTracker/AutoTrack_Min.lua
@@ -5,15 +5,12 @@ tcp:connect('127.0.0.1',54321)
 tcp:setoption('keepalive',true)
 
 local function checkKIs()
-	local words= {}
-	for i=0, 1 do
-		words[i]=memory.readdword(0x7E1514 + 4*i)
-	end
-	Bs=bit.band(memory.readdword(0x7E1500),0x1FFFF)
-	tcp:send(string.format("{\"KI\": %d,\"Loc1\": %d,\"Loc2\": %d}",Bs,words[0],words[1]))
+	local lowLOC = memory.readdword(0x7E1514)
+	local highLOC = memory.readdword(0x7E1518)
+	local KI = bit.band(memory.readdword(0x7E1500),0x1FFFF)
+	tcp:send(string.format("{\"KI\": %d,\"Loc1\": %d,\"Loc2\": %d}",KI,lowLOC,highLOC))
 end
 
-	
 local function myframe()
 	if(emu.framecount() % 300 == 0)
 		checkKIs()
@@ -21,7 +18,7 @@ local function myframe()
 end
 
 local function metaData()
-	local numBytes=memory.readbyte(0x3FF000)+memory.readbyte(0x3FF001)*256+memory.readbyte(0x3FF002)*256*256+memory.readbyte(0x3FF003)*256*256*256
+	local numBytes=memory.readdword(0x3FF000)
 	local str = ""
 	for i=1,numBytes-2 do
 		str = str .. string.char(memory.readbyte(0x3FF004+i))
@@ -30,5 +27,4 @@ local function metaData()
 end
 
 emu.registerbefore(myframe)
-
 tcp:send("{" .. metaData() .. "}")
